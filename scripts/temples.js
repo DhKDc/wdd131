450a8b71cf04a3c76f6d796c355341f33cdda9ab
Daniel Toledo <daniel.toledo.fuentessud@gmail.com>
2025-01-31 23:06:07 -0300

    Responsiveness working (?)

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,12 +1,24 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
 
 hamburger.addEventListener('click', () => {
+    menu.classList.toggle('show');
+
     if (menu.classList.contains('show')) {
-        menu.classList.remove('show');
-        hamburger.classList.remove('active');
+        menuIcon.style.opacity = '0';
+        closeIcon.style.opacity = '1';
     } else {
-        menu.classList.add('show');
-        hamburger.classList.add('active');
+        menuIcon.style.opacity = '1';
+        closeIcon.style.opacity = '0';
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
+        menu.classList.remove('show');
+        menuIcon.style.opacity = '1';
+        closeIcon.style.opacity = '0';
     }
 });
