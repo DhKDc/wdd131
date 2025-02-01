571b7e9c11a74e759bf013f66229baf74810ad00
Daniel Toledo <daniel.toledo.fuentessud@gmail.com>
2025-02-01 00:06:53 -0300

    solving issues with the menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,20 +5,6 @@ const closeIcon = document.querySelector('.close-icon');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('show');
-
-    if (menu.classList.contains('show')) {
-        menuIcon.style.opacity = '0';
-        closeIcon.style.opacity = '1';
-    } else {
-        menuIcon.style.opacity = '1';
-        closeIcon.style.opacity = '0';
-    }
-});
-
-document.addEventListener('click', (e) => {
-    if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
-        menu.classList.remove('show');
-        menuIcon.style.opacity = '1';
-        closeIcon.style.opacity = '0';
-    }
+    menuIcon.style.opacity = menu.classList.contains('show') ? '0' : '1';
+    closeIcon.style.opacity = menu.classList.contains('show') ? '1' : '0';
 });
